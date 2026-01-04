bdd113b3526aa6412ca838083ced34fd127667cc
sulemanaivideo-debug <sulemanaivideo@gmail.com>
2026-01-04 14:51:26 +0530

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import Inbox from "@/pages/Inbox";
+import EmailDetail from "@/pages/EmailDetail";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Inbox} />
+      <Route path="/email/:id" component={EmailDetail} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Router />
+        <Toaster />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
